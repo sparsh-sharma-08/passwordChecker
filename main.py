c7f26a731fb49b474771520925c4a842ffb2f9f3
sparsh-sharma-08 <69885649+sparsh-sharma-08@users.noreply.github.com>
2024-11-25 01:38:03 +0530

    main.py

--- main.py
+++ main.py
@@ -1,26 +1,40 @@
 ''' Make a password strength checker that checks the following parameters in a password:
-length, uppercase letters, lowercase letters, numbers, special characters, no-common words '''
+length, uppercase letters, lowercase letters, numbers, special characters, no-common words 
+also that tells the shortcomings of the password'''
 
 
 password = input("Enter your password: ")
 
-commonPass=('Password@1234', '1234', '9876', '0000', 'hello')
+commonPass=('Password@1234', '1234', '9876', '0000', 'hello', '')
 count=0 #initially set to 0
+shortcomings=[]
 
 
 #performs checks on each parameter
 if len(password)>=8:
     count+=1
+else:
+    shortcomings.append("Minimum characters needed:8")
 if any(char.isupper() for char in password):
     count+=1
+else:
+    shortcomings.append("Add one Uppercase Character")
 if any(char.islower() for char in password):
     count+=1
+else:
+    shortcomings.append("Add one LowerCase Character")
 if any(char.isdigit() for char in password):
     count+=1
+else:
+    shortcomings.append("Add one digit")
 if any(char in "!@#$%^&*()_+" for char in password):
     count+=1
+else:
+    shortcomings.append("Add one Unique Character")
 if not any(char in password for char in commonPass):
     count+=1
+else:
+    shortcomings.append("Too easy to guess")
 
 
 #Classification as veryStrong, strong, medium, weak
@@ -30,7 +44,9 @@ elif count<=4:
     print(f"Your Password is medium with a score of {count}")
 elif count==5:
     print(f"Your Password is secure with a score of {count}")
-if count==6:
+elif count==6:
     print(f"Your Password is very secure with a score of {count}")
-else:
-    print("Please redo the action")
+
+#Shows the list of the changes that you must do to get a secured password
+if count!=6:
+    print(shortcomings)
